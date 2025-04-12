70faa0f3c1d679d687a71b5a7f02636d03ec6200
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-04-12 21:36:08 +0900

    IPコマンドにボタンを追加し、環境変数からのIPとポートの取得を改善

--- discord_bot/commands/ip.js
+++ discord_bot/commands/ip.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} = require("discord.js");
 require("dotenv").config();
 
 module.exports = {
@@ -8,15 +14,24 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
+      let mcIP = process.env.mcIPaddress;
+      let mcPort = process.env.mcPort || 19132;
+
       let embed = new EmbedBuilder()
         .setTitle("💻統合版マインクラフトサーバーの接続情報")
-        .setDescription(
-          `- IP: \`${process.env.mcIPaddress}\`\n- PORT: \`${process.env.mcPort}\``
-        )
+        .setDescription(`- IP: \`${mcIP}\`\n- PORT: \`${mcPort}\``)
         .setColor(0x00ff00);
+
+      let button = new ActionRowBuilder(
+        new ButtonBuilder()
+          .setLabel("サーバーへの参加方法")
+          .setStyle(ButtonStyle.Link)
+          .setURL(`https://kuwa.app/tool/hjs/?ip=${mcIP}&port=${mcPort}`)
+      );
 
       return interaction.reply({
         embeds: [embed],
+        components: [button],
       });
     } catch (err) {
       const errorNotification = require("../errorNotification.js");
