2c552d111a1a517fd71e4c516c306a1b80c703ec
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-08-22 13:06:40 +0900

    コードのインデントを修正し、可読性を向上させました

--- discord_bot/events/clientReady.js
+++ discord_bot/events/clientReady.js
@@ -1,9 +1,9 @@
 const {
-  REST,
-  Routes,
-  ActivityType,
-  PresenceUpdateStatus,
-  EmbedBuilder,
+	REST,
+	Routes,
+	ActivityType,
+	PresenceUpdateStatus,
+	EmbedBuilder,
 } = require("discord.js");
 const mcpePing = require("mcpe-ping");
 const discord_token = process.env.discord_token;
@@ -13,55 +13,55 @@ const downNotifyChannelId = process.env.mcDownNotificationChannel;
 const readyChannelId = process.env.console_channel;
 
 module.exports = async (client) => {
-  //discord botへのコマンドの設定
-  const rest = new REST({ version: "10" }).setToken(discord_token);
-  (async () => {
-    try {
-      await rest.put(Routes.applicationCommands(client.user.id), {
-        body: await client.commands,
-      });
-      console.log("スラッシュコマンドの再読み込みに成功しました。");
-    } catch (err) {
-      console.log(
-        `❌ スラッシュコマンドの再読み込み時にエラーが発生しました。：\n${err}`
-      );
-    }
-  })();
+	//discord botへのコマンドの設定
+	const rest = new REST({ version: "10" }).setToken(discord_token);
+	(async () => {
+		try {
+			await rest.put(Routes.applicationCommands(client.user.id), {
+				body: await client.commands,
+			});
+			console.log("スラッシュコマンドの再読み込みに成功しました。");
+		} catch (err) {
+			console.log(
+				`❌ スラッシュコマンドの再読み込み時にエラーが発生しました。：\n${err}`
+			);
+		}
+	})();
 
-  console.log(`${client.user.username}への接続に成功しました。`);
+	console.log(`${client.user.username}への接続に成功しました。`);
 
-  //カスタマイズアクティビティを設定
-  let oldStatus;
-  setInterval(() => {
-    mcpePing(IPaddress, port, (err, res) => {
-      if (err) {
-        client.user.setActivity({
-          name: "サーバーがオフラインです",
-          type: ActivityType.Competing,
-        });
-        client.user.setStatus(PresenceUpdateStatus.Idle);
+	//カスタマイズアクティビティを設定
+	let oldStatus;
+	setInterval(() => {
+		mcpePing(IPaddress, port, (err, res) => {
+			if (err) {
+				client.user.setActivity({
+					name: "サーバーがオフラインです",
+					type: ActivityType.Competing,
+				});
+				client.user.setStatus(PresenceUpdateStatus.Idle);
 
-        if (oldStatus) {
-          let embed = new EmbedBuilder()
-            .setTitle("**⛔　サーバー停止**")
-            .setColor(0xff0000)
-            .setTimestamp();
-          client.channels.cache.get(downNotifyChannelId).send({
-            embeds: [embed],
-          });
-          oldStatus = false;
-        }
-        return;
-      }
+				if (oldStatus) {
+					let embed = new EmbedBuilder()
+						.setTitle("**⛔　サーバー停止**")
+						.setColor(0xff0000)
+						.setTimestamp();
+					client.channels.cache.get(downNotifyChannelId).send({
+						embeds: [embed],
+					});
+					oldStatus = false;
+				}
+				return;
+			}
 
-      client.user.setActivity({
-        name: `${res.currentPlayers}/${res.maxPlayers}人がオンラインです。`,
-        type: ActivityType.Competing,
-      });
-      client.user.setStatus(PresenceUpdateStatus.Online);
-      oldStatus = true;
-    });
-  }, 10000);
+			client.user.setActivity({
+				name: `${res.currentPlayers}/${res.maxPlayers}人がオンラインです。`,
+				type: ActivityType.Competing,
+			});
+			client.user.setStatus(PresenceUpdateStatus.Online);
+			oldStatus = true;
+		});
+	}, 10000);
 
-  client.channels.cache.get(readyChannelId).send("BOTを起動しました！");
+	client.channels.cache.get(readyChannelId).send("BOTを起動しました！");
 };
