ef8bf1874fb29bc97ae607e18a780c8c46f927a0
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2026-01-04 15:45:21 +0900

    fix: クリーキングとパーチドが原因の死亡メッセージを追加し、矢による死亡ログを修正し、一部の死亡ログが動作しない問題を修正

--- bedrock_addon/scripts/convertDieMessage.js
+++ bedrock_addon/scripts/convertDieMessage.js
@@ -113,6 +113,9 @@ export default async (damageSource) => {
 		// プレイヤーの攻撃で死んだ場合
 		if (damageSource.damagingEntity?.typeId == 'minecraft:player')
 			return 'プレイヤーに殺害された';
+		// クリーキングに殺された場合
+		if (damageSource.damagingEntity?.typeId == 'minecraft:creaking')
+			return 'クリーキングに殺害された';
 	}
 
 	// エンティティの爆発で死んだ場合
@@ -154,6 +157,58 @@ export default async (damageSource) => {
 	// 落雷で死んだ場合
 	if (damageSource.cause == 'lightning') return '雷に打たれた';
 
+	// 壁の中で窒息死した時
+	if (damageSource.cause == 'suffocation') return '壁の中で窒息した';
+
+	// マグマブロックで死んだ場合
+	if (damageSource.cause == 'magma') return '足元が溶岩だと気づいた';
+
+	// コマンドなどで死んだ場合
+	if (damageSource.cause == 'none') return '死んだ';
+
+	// ビヘイビアーパックの体力削りで死んだ場合
+	if (damageSource.cause == 'override') return '死んだ';
+
+	// ヤギの突進で死んだ場合
+	if (damageSource.cause == 'ramAttack') return '死んだ'; //'ヤギに突進されて力尽きた'が本来のはずだが、統合版内のログに合わせる
+
+	// killコマンドで死んだ場合
+	if (damageSource.cause == 'selfDestruct') return '死んだ';
+
+	// 衝撃波で死んだ場合
+	if (damageSource.cause == 'sonicBoom')
+		// ウォーデンの衝撃波で死んだ場合
+		return 'ウォーデンから逃れようとして衝撃波に消し飛ばされた';
+
+	// 奈落の底で死んだ場合
+	if (damageSource.cause == 'void') return '奈落の底へ落ちた';
+
+	// ウィザー効果で死んだ場合
+	if (damageSource.cause == 'wither') return '干からびた';
+
+	// 鍾乳石で死んだ場合
+	// 上から落ちてきた
+	if (damageSource.cause == 'stalactite')
+		return '落ちてきた鍾乳石に串刺しにされた';
+	// 足元
+	if (damageSource.cause == 'stalagmite') return '石筍に突き刺さった';
+
+	// 空腹で死んだ場合
+	if (damageSource.cause == 'starve') return '飢え死にした';
+
+	// トゲで死んだ場合
+	if (damageSource.cause == 'thorns') {
+		// ガーディアンのトゲで死んだ場合
+		if (damageSource.damagingEntity?.typeId == 'minecraft:guardian')
+			return 'ガーディアンを傷つけようとして殺害された';
+		// エルダーガーディアンのトゲで死んだ場合
+		if (damageSource.damagingEntity?.typeId == 'minecraft:elder_guardian')
+			return 'エルダーガーディアンを傷つけようとして殺害された';
+		// 棘の鎧で死んだ場合
+		if (damageSource.damagingEntity?.typeId == 'minecraft:player')
+			return '棘の鎧で反撃された';
+	}
+
 	// エフェクトで死んだ場合
 	if (damageSource.cause == 'magic') {
 		// ウィッチの魔法で死んだ場合
@@ -169,71 +224,27 @@ export default async (damageSource) => {
 		return '魔法で殺された';
 	}
 
-	// マグマブロックで死んだ場合
-	if (damageSource.cause == 'magma') return '足元が溶岩だと気づいた';
-
-	// コマンドなどで死んだ場合
-	if (damageSource.cause == 'none') return '死んだ';
-
-	// ビヘイビアーパックの体力削りで死んだ場合
-	if (damageSource.cause == 'override') return '死んだ';
-
 	// 飛翔物で死んだ場合
 	if (damageSource.cause == 'projectile') {
 		// 矢で死んだ場合
 		if (damageSource.damagingProjectile?.typeId == 'minecraft:arrow') {
 			// 矢の発射者次第で処理を変える
 			if (damageSource.damagingEntity?.typeId == 'minecraft:player')
-				return 'プレイヤーが射った矢に射抜かれた';
+				return 'プレイヤーに射抜かれた';
 			if (damageSource.damagingEntity?.typeId == 'minecraft:arrow')
-				return 'どこかから飛んできた矢に射抜かれた';
+				return '矢に殺害された';
 			if (damageSource.damagingEntity?.typeId == 'minecraft:skeleton')
-				return 'スケルトンが射った矢に射抜かれた';
+				return 'スケルトンに射抜かれた';
 			if (damageSource.damagingEntity?.typeId == 'minecraft:stray')
-				return 'ストレイが射った矢に射抜かれた';
+				return 'ストレイに射抜かれた';
 			if (damageSource.damagingEntity?.typeId == 'minecraft:pillager')
-				return 'ピリジャーが射った矢に射抜かれた';
+				return 'ピリジャーに射抜かれた';
 			if (damageSource.damagingEntity?.typeId == 'minecraft:piglin')
-				return 'ピグリンが射った矢に射抜かれた';
+				return 'ピグリンに射抜かれた';
 			if (damageSource.damagingEntity?.typeId == 'minecraft:bogged')
-				return 'ボグドが射った矢に射抜かれた';
-		}
-
-		// ヤギの突進で死んだ場合
-		if (damageSource.cause == 'ramAttack') return 'ヤギに突進されて力尽きた';
-
-		// killコマンドで死んだ場合
-		if (damageSource.cause == 'selfDestruct') return '死んだ';
-
-		// 衝撃波で死んだ場合
-		if (damageSource.cause == 'sonicBoom')
-			// ウォーデンの衝撃波で死んだ場合
-			return 'ウォーデンから逃れようとして衝撃波に消し飛ばされた';
-
-		// 鍾乳石で死んだ場合
-		// 上から落ちてきた
-		if (damageSource.cause == 'stalactite')
-			return '落ちてきた鍾乳石に串刺しにされた';
-		// 足元
-		if (damageSource.cause == 'stalagmite') return '鍾乳石に突き刺さった';
-
-		// 空腹で死んだ場合
-		if (damageSource.cause == 'starve') return '飢え死にした';
-
-		// 壁の中で窒息死した時
-		if (damageSource.cause == 'suffocation') return '壁の中で窒息した';
-
-		// トゲで死んだ場合
-		if (damageSource.cause == 'thorns') {
-			// ガーディアンのトゲで死んだ場合
-			if (damageSource.damagingEntity?.typeId == 'minecraft:guardian')
-				return 'ガーディアンを傷つけようとして殺害された';
-			// エルダーガーディアンのトゲで死んだ場合
-			if (damageSource.damagingEntity?.typeId == 'minecraft:elder_guardian')
-				return 'エルダーガーディアンを傷つけようとして殺害された';
-			// 棘の鎧で死んだ場合
-			if (damageSource.damagingEntity?.typeId == 'minecraft:player')
-				return '棘の鎧で反撃された';
+				return 'ボグドに射抜かれた';
+			if (damageSource.damagingEntity?.typeId == 'minecraft:parched')
+				return 'パーチドに射抜かれた';
 		}
 
 		// トライデントで死んだ場合
@@ -264,12 +275,6 @@ export default async (damageSource) => {
 			return 'ウィンドチャージに殺害され';
 		return '飛翔物に殺害された';
 	}
-
-	// 奈落の底で死んだ場合
-	if (damageSource.cause == 'void') return '奈落の底へ落ちた';
-
-	// ウィザー効果で死んだ場合
-	if (damageSource.cause == 'wither') return '干からびた';
 
 	return null;
 };
